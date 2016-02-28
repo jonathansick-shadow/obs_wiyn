c5cf6e883c62fc580a2c4adf89ccebdc0ccc6c11
Michael Wood-Vasey <wmwv@pitt.edu>
2016-02-28 16:19:26 -0500

    Working toward ingesting stacks for SweetSpot DR1.

--- tests/getRaw.py
+++ tests/getRaw.py
@@ -31,7 +31,7 @@ class GetRawTestCase(unittest.TestCase):
 
     def setUp(self):
         self.datadir = os.getenv("TESTDATA_WHIRC_DIR")
-        assert self.datadir, "testdata_whirc is not setup"
+        assert os.path.exists(self.datadir), "testdata_whirc is not setup"
         self.butler = getButler(self.datadir)
         self.size = (2144, 2050)
         self.dataId = {'date': 20111115,
